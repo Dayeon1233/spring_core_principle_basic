24b46675735caff050b943eebe71676068fb8dec
dayeon <swdy1233@gmail.com>
2021-07-28 18:39:12 +0900

    dip_ocp_srp적용 최종

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,27 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService(){
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+
+    }
+
+    private FixDiscountPolicy getDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,15 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
     //여기서 memberRepository는 MemberService와 MemoryMemberRepository에 의존하고 있다.
     //추상화와 구체화 모두에 의존중
 
+    private final MemberRepository memberRepository;
 
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
